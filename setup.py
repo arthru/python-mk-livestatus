d78500391c3f262d9b04b941bc3fd773db264b15
Arthur Vuillard <arthur@hashbang.fr>
2015-03-17 21:58:23 +0100

    use setuptools in setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python
-import os
-from distutils.core import setup
+
+from os.path import join, dirname
+from setuptools import setup
 
 import mk_livestatus
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='python-mk-livestatus',
-      version=mk_livestatus.__version__,
-      description='Helps to query MK livestatus and return results as dictionaries',
-      long_description=read('README.rst'),
-      author='Michael Fladischer',
-      author_email='michael@fladi.at',
-      url='https://github.com/arthru/python-mk-livestatus',
-      download_url='http://pypi.python.org/pypi/python-mk-livestatus',
-      packages=['mk_livestatus'],
-      license='BSD',
-      classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'Intended Audience :: System Administrators',
-            'License :: OSI Approved :: BSD License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'Topic :: System :: Monitoring',
-      ],
+def read(filename):
+    filepath = join(dirname(__file__), filename)
+    with open(filepath) as f:
+        return f.read()
+
+
+setup(
+    name='python-mk-livestatus',
+    version=mk_livestatus.__version__,
+    description='Helps to query MK livestatus and get results',
+    long_description=read('README.rst'),
+    author='Michael Fladischer',
+    author_email='michael@fladi.at',
+    url='https://github.com/arthru/python-mk-livestatus',
+    download_url='http://pypi.python.org/pypi/python-mk-livestatus',
+    packages=['mk_livestatus'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Monitoring',
+    ],
 )
